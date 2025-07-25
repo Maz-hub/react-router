46f0dacec43722a232b579594cb6552aabd72b7a
Maz <mazoolander@me.com>
2025-07-25 17:08:46 +0200

    Refactor App component to include NavBar and update routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import BrowserRouter from "./BrowserRouter.jsx";
 import Route from "./Route.jsx";
 import Blog from "./Blog.jsx";
+import NavBar from "./NavBar.jsx";
 
 import "./App.css";
 
@@ -8,14 +9,14 @@ function App() {
   return (
     <>
       <h1>App</h1>
+
       <BrowserRouter>
         <h2>Browser Router!</h2>
-        <Route path="/1">
-          <h3>Route 1</h3>
-          <a href="/2">Go to route 2</a>
+        <NavBar />
+        <Route path="/home">
+          <h3>Home</h3>
         </Route>
-        <Route path="/2">
-          <h3>Route 2</h3>
+        <Route path="/blog">
           <Blog />
         </Route>
       </BrowserRouter>
