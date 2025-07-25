1ab5e8c78c498fab3ae9deebbd4eb376e7dfb17b
Maz <mazoolander@me.com>
2025-07-25 15:05:20 +0200

    Add Blog component and integrate it into Route 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import BrowserRouter from "./BrowserRouter.jsx";
 import Route from "./Route.jsx";
+import Blog from "./Blog.jsx";
 
 import "./App.css";
 
@@ -11,6 +12,10 @@ function App() {
         <h2>Browser Router!</h2>
         <Route path="/1">
           <h3>Route 1</h3>
+        </Route>
+        <Route path="/2">
+          <h3>Route 2</h3>
+          <Blog />
         </Route>
       </BrowserRouter>
     </>
