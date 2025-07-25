519ad47477ff4255b8defc91fdf3838108715429
Maz <mazoolander@me.com>
2025-07-25 16:57:23 +0200

    Add Link component with navigation logic

--- src/Link.jsx
+++ src/Link.jsx
@@ -1,3 +1,11 @@
-export default function Link() {
-  return <></>;
+import { useContext } from "react";
+import { BrowserContext } from "./BrowserRouter.jsx";
+
+export default function Link({ children, to }) {
+  const { setCurrentURL } = useContext(BrowserContext);
+
+  function handleNavigation() {
+    setCurrentURL(new URL(window.location.origin + to));
+  }
+  return <a onClick={handleNavigation}>{children}</a>;
 }
